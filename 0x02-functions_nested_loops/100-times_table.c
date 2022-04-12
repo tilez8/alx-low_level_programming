952915ea6ccdc1974d959ad8508a385881f7dd1f
tilez8 <tilezeightz08@gmail.com>
2022-04-12 13:07:30 -0700

    Task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,49 @@
 #include "main.h"
 
 /**
-* print_times_table - Prints the n times table
-*
-* @n: number times table (0 < n <= 15)
-*
-* Return: no return
-*/
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
+ */
 void print_times_table(int n)
 {
-	int a, b, op;
+	int row;
+	int column;
+	int product;
 
 	if (n >= 0 && n <= 15)
 	{
-	for (a = 0; a <= n; a++)
-	{
-	_putchar(48);
-	for (b = 1; b <= n; b++)
-	{
-		op = a * b;
-		_putchar(44);
-		_putchar(32);
-		if (op <= 9)
+		for (row = 0; row <= n; row++)
 		{
-		_putchar(32);
-		_putchar(32);
-		_putchar(op + 48);
+			for (column = 0; column <= n; column++)
+			{
+				product = (row * column);
+				if (column == 0)
+					_putchar('0' + product);
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (product <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
+				}
+			}
+			_putchar('\n');
 		}
-		else if (op <= 99)
-		{
-		_putchar(32);
-		_putchar((op / 10) + 48);
-		_putchar((op % 10) + 48);
-		}
-		else
-		{
-		_putchar(((op / 100) % 10) + 48);
-		_putchar(((op / 10) % 10) + 48);
-		_putchar((op % 10) + 48);
-		}
-	}
-	_putchar('\n');
-	}
 	}
 }
-
