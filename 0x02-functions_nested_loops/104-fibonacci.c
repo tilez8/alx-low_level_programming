0e82ba3aab2751e7adfd3ea8914a7ccc773e9ae4
tilez8 <tilezeightz08@gmail.com>
2022-04-12 12:53:18 -0700

    Task16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	long n1 = 1;
+	long n2 = 1;
+
+	int i;
+	long sum;
+	long  aux = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (!(i == 0))
+		{
+			sum = n1 + n2;
+			n1 = n2;
+			n2 = sum;
+			if (sum < 4000000)
+			{
+				if ((sum % 2) == 0)
+				{
+					aux = aux + sum;
+				}
+			}
+		}
+	}
+	printf("%ld", aux);
+	printf("\n");
+	return (0);
+}
