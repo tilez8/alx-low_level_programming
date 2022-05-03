38759ca6ab639091b3af516c0d9944917c94651f
tilez8 <tilezeightz08@gmail.com>
2022-05-03 10:44:05 -0700

    Task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,39 +1,38 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * *str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: pointer to new space in memory or null
- **/
+ * str_concat - concatenates two strings.
+ * @s1: first string to be copied
+ * @s2: second string to be copied
+ *
+ * Return: newly allocated portion of memory containing s1 and s2
+ */
 char *str_concat(char *s1, char *s2)
 {
-	char *strDup;
-	int i, j;
+	char *concat;
+	int s1_len = 0, s2_len = 0;
+	int i, j, size;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = j = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	strDup = malloc(sizeof(char) * (i + j + 1));
-	if (strDup == NULL)
-		return (NULL);
-	i = j = 0;
-	while (s1[i] != '\0')
-	{
-		strDup[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		strDup[i] = s2[j];
-		i++, j++;
-	}
-	strDup[i] = '\0';
-	return (strDup);
-}
 
+	while (s1[s1_len] != '\0')
+		s1_len++;
+	while (s2[s2_len] != '\0')
+		s2_len++;
+
+	size = s1_len + s2_len;
+	concat = malloc(sizeof(char) * (size + 1));
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0; i < s1_len; i++)
+		concat[i] = s1[i];
+	for (j = 0; i < size && j < s2_len; i++, j++)
+		concat[i] = s2[j];
+	concat[i] = '\0';
+
+	return (concat);
+}
