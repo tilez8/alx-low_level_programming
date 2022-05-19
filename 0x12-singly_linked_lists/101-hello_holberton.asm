fffdfeef3bdcf87d5122fcbc03351c717f4456c8
tilez8 <tilezeightz08@gmail.com>
2022-05-19 10:14:54 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,11 @@
+; File: 101-hello_holberton.asm
+; Auth: Brennan D Baraban
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
+
+extern printf
+
+section .text
+   global main
+
+main:
