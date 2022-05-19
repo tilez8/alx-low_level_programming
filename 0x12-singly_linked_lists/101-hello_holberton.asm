971ba4d269ed326476fa79d5c25e7ebccddfc46b
tilez8 <tilezeightz08@gmail.com>
2022-05-19 10:22:09 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,22 @@
-; File: 101-hello_holberton.asm
-; Auth: Brennan D Baraban
-; Desc: 64-bit assembly program that prints
-;       Hello, Holberton followed by a new line.
+; Declare needed C  functions
+	extern	printf		; the C function, to be called
 
-extern printf
+	section .data		; Data section, initialized variables
+msg:	db "Hello, Holberton", 0; C string needs 0
+fmt:	db "%s", 10, 0		; The printf format, "\n",'0'
 
-section .text
-   global main
+	section .text		; Code section.
 
-main:
+	global main		; the standard gcc entry point
+main:				; the program label for the entry point
+	push	rbp		; set up stack frame, must be alligned
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+	call	printf		; Call C function
+
+	pop	rbp		; restore stack
+
+	mov	rax,0		; normal, no error, return value
+	ret			; return
